75b0e16c034aa9e8d8f167a609d1a1612d6a61ed
Matias Camiletti <matias.camiletti@gmail.com>
2021-11-18 07:54:10 -0300

    Add with counts custom in fetch.

--- src/Handler/FetchHandler.php
+++ src/Handler/FetchHandler.php
@@ -40,8 +40,27 @@ class FetchHandler extends \Mia\Core\Request\MiaRequestHandler
     {
         // Obtenemos ID si fue enviado
         $itemId = $this->getParam($request, 'id', '');
-        // Buscar si existe el tour en la DB
-        $item = MIAPost::find($itemId);
+        // Verify has withs in query
+        $withs = $this->getParam($request, 'withs', '');
+        $countWiths = $this->getParam($request, 'count_withs', '');
+        if($countWiths != ''){
+            // Convert to array
+            $with = explode(',', $countWiths);
+            // Search item in DB
+            $item = MIAPost::with($with)->where('id', $itemId)->first();
+        } else if($withs != ''){
+            $query = MIAPost::where('id', $itemId);
+            // Convert to array
+            $withs = explode(',', $withs);
+            foreach($withs as $wc){ 
+                $query->withCount($wc);
+            }
+            // Search item in DB
+            $item = $query->first();
+        } else {
+            // Buscar si existe el tour en la DB
+            $item = MIAPost::find($itemId);
+        }
         // verificar si existe
         if($item === null){
             return new \Mia\Core\Diactoros\MiaJsonErrorResponse(1, 'This element not exist.');
